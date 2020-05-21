cbcdba1cae2c1484c1bbc2279b6fbdbf9a920fd0
Demario Douce <demario.douce567@gmail.com>
2020-05-21 18:26:33 -0400

    added the variables for the main second view.

---
+++ Tipsy/Controller/MainScreenViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MainScreenViewController.swift
+//  Tipsy
+//
+//  Created by Demario Douce on 2020-05-21.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class MainScreenViewController: UIViewController {
+
+    //UI elements
+    @IBOutlet weak var billTotalTbx: UITextField!
+    @IBOutlet weak var splitNum: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    //Func for the tip selected btn
+    @IBAction func tipSelected(_ sender: UIButton) {
+    }
+    
+    //Func to increase or decrease the splitNum
+    @IBAction func splitValueSelecter(_ sender: UIStepper) {
+    }
+    
+    //Func it does what it states which is calculate
+    @IBAction func CalBtn(_ sender: UIButton) {
+    }
+    
+}
