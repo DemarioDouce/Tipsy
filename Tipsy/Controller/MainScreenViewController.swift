3ff44f72f51b3fb097d44a0d65180f2fecd9ac39
Demario Douce <demario.douce567@gmail.com>
2020-05-21 20:04:57 -0400

    Second screen was able to open.

--- Tipsy/Controller/MainScreenViewController.swift
+++ Tipsy/Controller/MainScreenViewController.swift
@@ -9,29 +9,93 @@
 import UIKit
 
 class MainScreenViewController: UIViewController {
-
+    
     //UI elements
     @IBOutlet weak var billTotalTbx: UITextField!
     @IBOutlet weak var splitNum: UILabel!
+    @IBOutlet weak var tipSelectedBtn1: UIButton!
+    @IBOutlet weak var tipSelectedBtn2: UIButton!
+    @IBOutlet weak var tipSelectedBtn3: UIButton!
+    
+    //Variables
+    var billTotal: Float?
+    var tipPercentage = 0.0
+    var splitNumVal = 0
+    var billTotalCal = 0.0
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         
     }
     
-    //Func for the tip selected btn
-    @IBAction func tipSelected(_ sender: UIButton) {
+    //First tip btn
+    @IBAction func tipSelected1(_ sender: UIButton) {
+        
+        sender.isSelected = true
+        tipSelectedBtn2.isSelected = false
+        tipSelectedBtn3.isSelected = false
+        
+        
+        tipPercentage = 0.0
+        
+        print(tipPercentage)
     }
+    
+    //Second tip btn
+    @IBAction func tipSelected2(_ sender: UIButton) {
+        
+        sender.isSelected = true
+        tipSelectedBtn1.isSelected = false
+        tipSelectedBtn3.isSelected = false
+        
+        tipPercentage = 0.1
+        
+        print(tipPercentage)
+    }
+    
+    //Third tip btn
+    @IBAction func tipSelected3(_ sender: UIButton) {
+        
+        sender.isSelected = true
+        tipSelectedBtn1.isSelected = false
+        tipSelectedBtn2.isSelected = false
+        
+        tipPercentage = 0.2
+        
+        print(tipPercentage)
+    }
+    
+    
     
     //Func to increase or decrease the splitNum
     @IBAction func splitValueSelecter(_ sender: UIStepper) {
+        
+        splitNumVal =  Int(sender.value)
+        splitNum.text = splitNumVal.description
+        
     }
     
     //Func it does what it states which is calculate
     @IBAction func CalBtn(_ sender: UIButton) {
+        
+        billTotal = Float(billTotalTbx.text!)
+        
+        print(billTotal ?? 0.0)
+        
+        //Open segue
+        self.performSegue(withIdentifier: "showResult", sender: self)
+    }
+    
+    //Notifies the view controller that a segue is about to be performed.
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showResult"{
+            
+            let destinationVC = segue.destination as! ResultViewController
+            
+        }
     }
     
 }
---
+++ Tipsy/Controller/ResultViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ResultViewController.swift
+//  Tipsy
+//
+//  Created by Demario Douce on 2020-05-21.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    //UI Elements
+    @IBOutlet weak var resultLbl: UILabel!
+    @IBOutlet weak var resultDetailLbl: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
